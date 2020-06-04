101e33b4baf5aef637a2c77a028f1607e51be07b
Tomás Aprile <tomas.aprile@fnetsystem.com.ar>
2020-06-04 16:30:16 -0300

    init

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button @click="increment">Cantidad de clicks: {{ count }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment () {
+      this.count += 1
+    }
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+import BaseButton from './components/BaseButton.vue'
+
+export default BaseButton
